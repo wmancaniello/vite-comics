1a85f7e2ef41d7f35a018c44b4d4102b5633aadd
wmancaniello <wmancaniello@gmail.com>
2024-05-02 19:49:52 +0200

    fix link

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,44 +9,44 @@ export default {};
         <div class="left">
           <h2>DC COMICS</h2>
           <ul>
-            <li><a href="">Characters</a></li>
-            <li><a href="">Comics</a></li>
-            <li><a href="">Movies</a></li>
-            <li><a href="">TV</a></li>
-            <li><a href="">Games</a></li>
-            <li><a href="">Videos</a></li>
-            <li><a href="">News</a></li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
           </ul>
           <h2>SHOP</h2>
           <ul>
-            <li><a href="">Shop DC</a></li>
-            <li><a href="">Shop DC Collectibles</a></li>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
           </ul>
         </div>
         <div class="middle">
           <h2>DC</h2>
           <ul>
-            <li><a href="">Terms Of Use</a></li>
-            <li><a href="">Privacy policy (New)</a></li>
-            <li><a href="">Ad Choices</a></li>
-            <li><a href="">Advertising</a></li>
-            <li><a href="">Jobs</a></li>
-            <li><a href="">Subscriptions</a></li>
-            <li><a href="">Talent Workshops</a></li>
-            <li><a href="">CPSC Certificates</a></li>
-            <li><a href="">Ratings</a></li>
-            <li><a href="">Shop Help</a></li>
-            <li><a href="">Contact Us</a></li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
           </ul>
         </div>
         <div class="right">
           <h2>SITES</h2>
           <ul>
-            <li><a href="">DC</a></li>
-            <li><a href="">MAD Magazine</a></li>
-            <li><a href="">DC Kids</a></li>
-            <li><a href="">DC Universe</a></li>
-            <li><a href="">DC power Visa</a></li>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC power Visa</a></li>
           </ul>
         </div>
       </div>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,20 +5,20 @@ export default {};
 <template>
   <div class="header">
     <div class="logo">
-      <img src="../assets/img/dc-logo.png" alt="" />
+      <img src="../assets/img/dc-logo.png" alt="logo" />
     </div>
     <div class="menu-list">
       <ul>
-        <li><a href="">CHARACTERS</a></li>
-        <li><a href="">COMICS</a></li>
-        <li><a href="">MOVIES</a></li>
-        <li><a href="">TV</a></li>
-        <li><a href="">GAMES</a></li>
-        <li><a href="">COLLECTIBLES</a></li>
-        <li><a href="">VIDEOS</a></li>
-        <li><a href="">FANS</a></li>
-        <li><a href="">NEWS</a></li>
-        <li><a href="">SHOP</a></li>
+        <li><a href="#">CHARACTERS</a></li>
+        <li><a href="#">COMICS</a></li>
+        <li><a href="#">MOVIES</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">GAMES</a></li>
+        <li><a href="#">COLLECTIBLES</a></li>
+        <li><a href="#">VIDEOS</a></li>
+        <li><a href="#">FANS</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">SHOP</a></li>
       </ul>
     </div>
   </div>
